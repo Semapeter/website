822b805c777ed8362c7d0621b1998dcfb1aafb00
Semapeter <40825703+Semapeter@users.noreply.github.com>
2025-01-17 14:12:11 +0800

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -3,6 +3,6 @@ export const navigationItems = [
   { href: '/blog', text: '博客' },
   { href: '/projects', text: '项目' },
   { href: '/guestbook', text: '留言墙' },
-  { href: '/ama', text: 'AMA 咨询' },
+  { href: 'https://luoboy.com/', text: '博客网站' },
   // { href: '/about', text: '关于' },
 ]
